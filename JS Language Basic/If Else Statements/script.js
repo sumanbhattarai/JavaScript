580209960e21971b2a5d68aa575c4e048b2214fe
Suman Bhattarai <33854515+sumanbhattarai@users.noreply.github.com>
2019-01-07 21:31:21 +0545

    IF Else Statements

---
+++ JS Language Basic/If Else Statements/script.js
@@ -0,0 +1,11 @@
+var name = prompt('Enter your name : ') ;
+var age = prompt('Hey '+ name + ' Please enter your age : ') ;
+
+if(age>=18)
+{
+    alert('Hey '+name + '. You are an adult.') ;
+}
+else
+{
+    alert('Hey '+name + '. Soryy ,You are not an adult.');
+}
