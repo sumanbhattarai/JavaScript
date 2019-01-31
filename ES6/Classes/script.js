61295be8bd092073747e85d6977b5440ccbc9758
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-01-31 18:24:45 +0545

    Initial commit

---
+++ ES6/Classes/script.js
@@ -0,0 +1,55 @@
+
+
+// Function Constructors in ES5
+
+var info = function(name , gender , dob) {
+    this.name = name ;
+    this.gender  = gender ;
+    this.dob = dob ;
+}
+
+// Prototype properties of function Constructors
+
+info.prototype.calcage = function( dob)
+{
+    console.log( 2018 - this.dob ) ;
+}
+
+var SB = new info('Suman' , 'male' , 1999 );
+SB.calcage();
+
+
+// Using classes in ES6 
+
+class detail {
+    constructor(name , gender , dob )
+    {
+        this.name = name ;
+        this.gender  = gender ;
+        this.dob = dob ;
+
+    }
+
+    calcage()
+    {
+        console.log( 2018 - this.dob ) ;
+    }
+
+    // Lets create a static method here.
+    // Note : static method can never be inherited to instances.
+
+    static try()
+    {
+        console.log(' I am static method.') ;
+    }
+}
+
+// Creating the instance of this class.
+
+const std = new detail('Sujan' , 'male' , 1998 ) ;
+std.calcage();
+
+ // std.try(); // It will throw an error since instances donot have access to the static method.
+ // We can call the static method as :
+
+ detail.try();
