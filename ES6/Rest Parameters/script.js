9eaa18ab15b72d32c637f5100e5e6c477f3baee0
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-01-31 17:01:04 +0545

    Added limt to the function

--- ES6/Rest Parameters/script.js
+++ ES6/Rest Parameters/script.js
@@ -25,3 +25,30 @@ function data6( ...input )
     console.log(input) ;
 }
 data6(3, 43, 54, 234) ;
+
+
+//ES5
+
+function isAdult(limit)
+{
+    var arr = Array.prototype.slice.call( arguments  , 1 ) ; // We did this to exclude 18.
+    arr.forEach(function(el){
+        console.log( el >= limit);
+    });
+    
+
+}
+
+isAdult(18 , 20, 32 , 12 , 45 , 15 , 7) ;
+
+
+// ES6
+
+function isAdult6 (limit , ...years )
+{
+    years.forEach( el => console.log(el >= limit));
+}
+
+isAdult6(18 , 20 , 32 , 12 , 45 , 15 , 7) ;
+
+
