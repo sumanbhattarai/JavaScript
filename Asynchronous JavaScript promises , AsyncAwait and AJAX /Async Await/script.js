9b4926e20b058c9d828226d66d4eec38a07e6cb9
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-02 20:58:01 +0545

    Used Async Await while consuming the promises

--- Asynchronous JavaScript promises , AsyncAwait and AJAX /Async Await/script.js
+++ Asynchronous JavaScript promises , AsyncAwait and AJAX /Async Await/script.js
@@ -4,3 +4,33 @@
 // It's surprisingly easy to understand and use.
 // The async function declaration defines an asynchronous function, which returns an AsyncFunction object.
 // It was introduced in ES8 .
+
+const getUp = new Promise((resolve , reject) =>{
+    resolve('I got Up') ;
+});
+
+
+const brushTeeth = function(message){
+return new Promise((resolve , reject)=>{
+    resolve(`${message} and I brushed my teeth`) ;
+});
+}
+
+const wentCollege = function(message){
+return  new Promise((resolve,reject)=>{
+    resolve(`${message} and I went to the College.`) ;
+
+});
+}
+
+
+async function task()
+{
+     const getUpmessage = await getUp ;
+     const brushTeethMessage = await brushTeeth(getUpmessage) ;
+     const result = await wentCollege(brushTeethMessage);
+     console.log(result);  
+
+}
+
+task();
