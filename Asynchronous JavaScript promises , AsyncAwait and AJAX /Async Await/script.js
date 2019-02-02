5aa2be9508962acd6da7da9d25db96b1ec937694
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-02 20:48:07 +0545

    Added into

---
+++ Asynchronous JavaScript promises , AsyncAwait and AJAX /Async Await/script.js
@@ -0,0 +1,6 @@
+
+
+// It is a special syntax to work with promises in a more comfortable fashion.
+// It's surprisingly easy to understand and use.
+// The async function declaration defines an asynchronous function, which returns an AsyncFunction object.
+// It was introduced in ES8 .
