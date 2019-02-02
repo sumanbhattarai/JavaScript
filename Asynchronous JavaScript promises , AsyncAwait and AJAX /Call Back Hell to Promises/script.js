5bb004e4050f61cbb7ba79424bad052aea8628aa
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-02 18:39:47 +0545

    Simplifying the call back hell into promises

--- Asynchronous JavaScript promises , AsyncAwait and AJAX /Call Back Hell to Promises/script.js
+++ Asynchronous JavaScript promises , AsyncAwait and AJAX /Call Back Hell to Promises/script.js
@@ -28,5 +28,36 @@ function task()
 task();
 
 
+// Call back hell to promises
+
+const getUp = new Promise((resolve , reject) =>{
+    resolve('I got Up') ;
+});
+
+
+const brushTeeth = function(){
+return new Promise((resolve , reject)=>{
+    resolve('I brushed my teeth') ;
+});
+}
+
+const wentCollege = function(){
+return  new Promise((resolve,reject)=>{
+    resolve('I went to College') ;
+
+});
+}
+
+getUp.then((message)=>{
+console.log(message);
+return brushTeeth();
+}).then((message1)=>{
+console.log(message1) ;
+return wentCollege(); 
+}).then((message2)=>{
+console.log(message2) ;
+}) ;
+
+
 
 
