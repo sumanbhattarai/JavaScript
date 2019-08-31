a1d7e36ee451878be355f397d4bb21619b59dccd
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-08-31 19:18:17 +0545

    Initial Commit

---
+++ Cool Tricks/random.js
@@ -0,0 +1,7 @@
+
+const x = 10 
+console.log(typeof (typeof x ))
+// It gives string since typeof x is number and typeof number is string.
+
+console.log([]==[])
+// It is false since array is also an object in JS
