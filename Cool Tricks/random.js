fb99de113ad2544183007942c72b8a3b5c25d2fb
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-10-11 08:34:08 +0545

    typeof

--- Cool Tricks/random.js
+++ Cool Tricks/random.js
@@ -8,3 +8,22 @@ console.log([]==[])
 
 console.log("55"+5); // It gives 555.
 console.log("55"-5); // It gives 50.
+
+function sum()
+{
+    return true;
+}
+console.log(typeof sum); // Function
+console.log(typeof sum()); // boolean
+
+function demo()
+{
+    return(()=>'suman');
+}
+console.log(typeof demo()); // function
+
+function demo2()
+{
+    return(()=>1)();
+}
+console.log(typeof demo2()); // number
